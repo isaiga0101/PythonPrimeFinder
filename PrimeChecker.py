7e8f83602a7b110ac24f715c9aecb697b6510dbb
Isaiah Gayfield <igayfield7@gmail.com>
2016-07-21 09:36:05 -0700

    The Def for Checking a Number for Primality

---
+++ PrimeChecker.py
@@ -0,0 +1,30 @@
+#This handles checking if a number is prime
+def isprime(n):
+	print("Prime Checker")
+	print("programed by: Isaiah Gayfield")
+	print("Start Date: 7/20/2016")
+	print("Version: 0.0.0")
+	'''check if integer n is a prime'''
+
+	# make sure n is a positive integer
+	n = abs(int(n))
+
+	# 0 and 1 are not primes
+	if n < 2:
+		return False
+
+	# 2 is the only even prime number
+	if n == 2: 
+		return True    
+
+	# all other even numbers are not primes
+	if not n & 1: 
+		return False
+
+	# range starts with 3 and only needs to go up 
+	# the square root of n for all odd numbers
+	for x in range(3, int(n**0.5) + 1, 2):
+		if n % x == 0:
+			return False
+
+return True
