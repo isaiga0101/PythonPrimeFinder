32d39231866ea69ac296b773e27b7cff2949a391
Isaiah Gayfield <igayfield7@gmail.com>
2016-07-26 10:43:34 -0700

    Working Commit of Server

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import time
 import threading
 import sys
 
+clientCk = 0
+clientDone = 0
+
 # Calculates a percentage
 def percent(num,total):
 	holder = num/total
@@ -47,15 +50,18 @@ def isprime(n):
 	return True
 
 class client_thread(threading.Thread):
-        def __init__(self,threadSocket, Address,intPrime,top,bottom):
+        def __init__(self,threadSocket, Address,intPrime,top,bottom,num):
                 threading.Thread.__init__(self)
                 self.threadSocket = threadSocket
                 self.Address = Address
                 self.intPrime = intPrime
                 self.top = top
                 self.bottom = bottom
+                self.num = num
 
         def run(self):
+                global clientCk
+                global clientDone
                 errorCnt = 0
                 print('New thread created for connection to ' + str(self.Address))
                 while True:
@@ -115,37 +121,39 @@ class client_thread(threading.Thread):
                                 break
                         break
                 data = self.threadSocket.recv(1024).decode()
-                while True:
-                        if not data:
-                                break
-                        # Check if client sent true or false
-                        global clientCk
-                        if data.upper() == 'TRUE' and clientCk != -1:
-                                global clientCk
-                                clientCk = clientCk + 1
-                                clientDone = clientDone + 1
-                                break
-                        else:
-                                global clientCk
-                                clientCk = -1
-                                global clientDone
-                                clientDone = clientDone + 1
-                                break
+                # Check if client sent true or false
+                if data.upper() == 'TRUE' and clientCk != -1:
+                        clientCk = clientCk + 1
+                        clientDone = clientDone + 1
+                else:
+                        clientDone = clientDone + 1
+                        clientCk = -1
 
-                print('Connection closed to client' + str(self.Address))
-                self.threadSocket.close()
+                if clientDone == self.num:
+                        if clientCk > 0:
+                                print('The Number is Prime')
+                                self.threadSocket.close()
+                                sys.exit()
+                        else:
+                                print('The Number is NOT Prime')
+                                self.threadSocket.close()
+                                sys.exit()
+                else:
+                        print('Connection closed to client' + str(self.Address))
+                        self.threadSocket.close()
 
 #Main --------------------------------------------------------------------------------------
 print("Prime Checker")
 print("programed by: Isaiah Gayfield")
 print("Start Date: 7/20/2016")
 print("Version: 0.1.0")
+
 while True:
-        clientDone = 0
+
         low = 0
         high = 0
-        global clientCk
-        clientCk = 0
+        count = 0
+
         check = input('Enter \'Exit\' to exit, otherwise input number: ')
         if check.upper() == 'EXIT':
                 break
@@ -166,27 +174,26 @@ while True:
                 mySocket.bind(('', 0))
                 print ('Server is at: '+ socket.gethostname()+ ':' + str(mySocket.getsockname()[1]))
 
-                mySocket.listen(num)
+                mySocket.listen(5)
                 while True:
                         #Accept connections from outside
                         (clientsocket, address) = mySocket.accept()
                         #Start a thead for the client socket
-                        ct = client_thread(clientsocket,address,check,high,low)
+                        ct = client_thread(clientsocket,address,check,high,low,num)
                         ct.start()
-
-                        if high == check:
+                        count = count + 1
+                        if count == num:
                                 break
                         else:
                                 low = low + add
                                 high = high + add
-                global clientCk
-                while clientCk <= num:
-                        if num < 0:
-                                print(str(check)+" is not prime.")
-                                break
-                if num >= 0:
-                        print(str(check)+' is prime')
-
+                print('All clients have connected.')
+                wait = True
+                while wait == True:
+                        wait = True
+                while True:
+                        break
+                        
         else:
                 # This handles console IO
                 # Initialize variable if there is no input.
